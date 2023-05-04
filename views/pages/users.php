c0e3df97edc453a1f1d345c954e8b6653cf60da5
coderoycc <90710352+coderoycc@users.noreply.github.com>
2023-05-03 23:09:35 -0400

    modularizando con MVC2

---
+++ views/pages/users.php
@@ -0,0 +1,78 @@
+<div class="content-wrapper" style="min-height: 717px">
+  <section class="contenet-wrapper">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h3>Administración de Usuarios</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div class="card card-info card-outline">
+            <div class="card-header">
+              <button type="button" class="btn btn-success crear-perfil" data-toggle="modal" data-target="#modal-crear-perfil">Crear nuevo usuario</button><br>
+            </div>
+            <div class="card-body">
+              <table class="table table-bordered table-striped dt-responsive tablaperfil" width="100%">
+                <thead>
+                  <tr>
+                    <th style="width:10px">#</th>
+                    <th>NOMBRE</th>
+                    <th>USUARIO</th>
+                    <th>FOTO</th>
+                    <th>ROL</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+<div class="content-wrapper" style="min-height: 717px">
+  <section class="contenet-wrapper">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h3>Administración de Usuarios</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div class="card card-info card-outline">
+            <div class="card-header">
+              <button type="button" class="btn btn-success crear-perfil" data-toggle="modal" data-target="#modal-crear-perfil">Crear nuevo usuario</button><br>
+            </div>
+            <div class="card-body">
+              <table class="table table-bordered table-striped dt-responsive tablaperfil" width="100%">
+                <thead>
+                  <tr>
+                    <th style="width:10px">#</th>
+                    <th>NOMBRE</th>
+                    <th>USUARIO</th>
+                    <th>FOTO</th>
+                    <th>ROL</th>
+                    <th>ACCIONES</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
